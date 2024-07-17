bb79bd524b0900128a63130243faa1698c683ce9
mrKuncay008 <kuncaymuhammad@gmail.com>
2024-07-17 12:08:31 +0700

    Update 17 july

--- PHP/MVC/app/core/App.php
+++ PHP/MVC/app/core/App.php
@@ -1,9 +1,18 @@
 <?php
 
 class App {
+    protected $control = 'home';
+    protected $method = 'index';
+    protected $params = [];
+
     public function __construct() {
         $url = $this->parseURL();
-        var_dump($url);
+
+        if (file_exists('../app/control/').$url[0].'.php') {
+            $this->control = $url[0];
+            unset($url[0]);
+            var_dump($url);
+        }
     }
     public function parseURL(){
         if (isset($_GET['url'])) {
